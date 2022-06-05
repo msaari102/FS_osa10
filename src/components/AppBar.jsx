18dfb9a073a40c7b067d18e0f314df852ac1a28f
msaari102 <mttsaari@gmail.com>
2022-06-05 18:23:39 +0300

    Fullstack open 2022 osa 10.6

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,27 +1,30 @@
-import { View, StyleSheet, Text, Pressable } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
+import { Link } from "react-router-native";
 import Constants from 'expo-constants';
 import theme from '../theme';
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'row',
     paddingTop: Constants.statusBarHeight,
     backgroundColor: theme.colors.appbar,
-    height: 60,
+    
+    flexShrink: 1,
   },
   bartext: {
     color: theme.colors.bartext,
+    padding: 10,
   }
   // ...
 });
 
 const AppBar = () => {
-  return <View style={styles.container}>{
-    <Pressable onPress={() => {
-     console.log("pressed");
-    }}>
-      <Text style={styles.bartext}>Repositories</Text>
-    </Pressable>
-    }</View>;
+  return (
+    <View style={styles.container}>
+      <Link to="/"><Text style={styles.bartext}>Repository list</Text></Link>
+      <Link to="/signin"><Text style={styles.bartext}>Sign in</Text></Link>
+    </View>
+  )
 };
 
 export default AppBar;
